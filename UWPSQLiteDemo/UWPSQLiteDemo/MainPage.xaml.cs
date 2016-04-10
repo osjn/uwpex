a405e76ec127d94031b56378fabdafe14cb3d327
Jingnen <osjn@outlook.com>
2016-04-10 22:04:03 +0800

    add

--- UWPSQLiteDemo/UWPSQLiteDemo/MainPage.xaml.cs
+++ UWPSQLiteDemo/UWPSQLiteDemo/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using SQLite.Net.Attributes;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +23,49 @@ namespace UWPSQLiteDemo
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        string path;
+        SQLite.Net.SQLiteConnection conn;
+
         public MainPage()
         {
             this.InitializeComponent();
+            path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "db.sqlite");
+            conn = new SQLite.Net.SQLiteConnection(new SQLite.Net.Platform.WinRT.SQLitePlatformWinRT(), path);
+            conn.CreateTable<Customer>();
         }
+
+        private void Retrieve_Click(object sender, RoutedEventArgs e)
+        {
+            var query = conn.Table<Customer>();
+            string id = "";
+            string name = "";
+            string age = "";
+
+            foreach (var message in query)
+            {
+                id = id + " " + message.Id;
+                name = name + " " + message.Name;
+                age = age + " " + message.Age;
+            }
+
+            textBlock2.Text = "ID: " + id + "\nName: " + name + "\nAge: " + age;
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            var s = conn.Insert(new Customer()
+            {
+                Name = textBox.Text,
+                Age = textBox1.Text
+            });
+        }
+    }
+
+    public class Customer
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Age { get; set; }
     }
 }
