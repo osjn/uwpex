3cde4b44107c092bd70f44ab245b4aff947a0e54
Jingnen <osjn@outlook.com>
2016-04-11 17:31:19 +0800

    add

---
+++ AppServiceProvider/ClientApp/MainPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Windows.ApplicationModel.AppService;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ClientApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private AppServiceConnection inventoryService;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            // add the connection
+            if (this.inventoryService == null)
+            {
+                this.inventoryService = new AppServiceConnection();
+                this.inventoryService.AppServiceName = "com.microsoft.inventory";
+                this.inventoryService.PackageFamilyName = "bb1a8478-8005-46869923-e525ceaa26fc_4sz2ag3dcq60a";
+
+                var status = await this.inventoryService.OpenAsync();
+
+                if (status != AppServiceConnectionStatus.Success)
+                {
+                    button.Content = "Failed to connect";
+                    return;
+                }
+            }
+
+            // Call the service. 
+            int idx = int.Parse(textBox.Text);
+            var message = new ValueSet();
+
+            message.Add("Command", "Item");
+            message.Add("ID", idx);
+
+            AppServiceResponse response = await
+               this.inventoryService.SendMessageAsync(message);
+            string result = "";
+
+            if (response.Status == AppServiceResponseStatus.Success)
+            {
+                // Get the data  that the service sent  to us. 
+                if (response.Message["Status"] as string == "OK")
+                {
+                    result = response.Message["Result"] as string;
+                }
+            }
+
+            message.Clear();
+            message.Add("Command", "Price");
+            message.Add("ID", idx);
+
+            response = await this.inventoryService.SendMessageAsync(message);
+
+            if (response.Status == AppServiceResponseStatus.Success)
+            {
+                // Get the data that the service sent to us. 
+                if (response.Message["Status"] as string == "OK")
+                {
+                    result += " : Price = " + "$" + response.Message["Result"] as string;
+                }
+            }
+
+            textBlock.Text = result;
+        }
+    }
+}
---
+++ AppServiceProvider/MyAppService/Inventory.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Windows.ApplicationModel.AppService;
+using Windows.ApplicationModel.Background;
+using Windows.Foundation.Collections;
+
+namespace MyAppService
+{
+    public sealed class Inventory : IBackgroundTask
+    {
+        private BackgroundTaskDeferral backgroundTaskDeferral;
+        private AppServiceConnection appServiceconnection;
+
+        private String[] inventoryItems = new string[] { "Robot vacuum", "Chair" };
+        private double[] inventoryPrices = new double[] { 129.99, 88.99 };
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            this.backgroundTaskDeferral = taskInstance.GetDeferral();
+            taskInstance.Canceled += OnTaskCanceled;
+            var details = taskInstance.TriggerDetails as AppServiceTriggerDetails;
+
+            appServiceconnection = details.AppServiceConnection;
+            appServiceconnection.RequestReceived += OnRequestReceived;
+        }
+
+        private void OnTaskCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
+            if (this.backgroundTaskDeferral != null)
+            {
+                // Complete the service deferral. 
+                this.backgroundTaskDeferral.Complete();
+            }
+        }
+
+        private async void OnRequestReceived(AppServiceConnection sender, AppServiceRequestReceivedEventArgs args)
+        {
+            var messageDeferral = args.GetDeferral();
+            ValueSet message = args.Request.Message;
+            ValueSet returnData = new ValueSet();
+
+            string command = message["Command"] as string;
+            int? inventoryIndex = message["ID"] as int?;
+            if (inventoryIndex.HasValue
+                && inventoryIndex.Value >= 0
+                && inventoryIndex.Value < inventoryItems.GetLength(0))
+            {
+                switch (command)
+                {
+
+                    case "Price":
+                        returnData.Add("Result", inventoryPrices[inventoryIndex.Value]);
+                        returnData.Add("Status", "OK");
+                        break;
+
+                    case "Item":
+                        returnData.Add("Result", inventoryItems[inventoryIndex.Value]);
+                        returnData.Add("Status", "OK");
+                        break;
+
+                    default:
+                        returnData.Add("Status", "Fail: unknown command");
+                        break;
+                }
+            }
+            else
+            {
+                returnData.Add("Status", "Fail: Index out of range");
+            }
+
+            await args.Request.SendResponseAsync(returnData);
+            messageDeferral.Complete();
+        }
+    }
+}
